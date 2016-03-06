4e3c086943eb6230808c66af77013eeabf4dfe16
David Pugal <david.pugal@gmail.com>
2016-03-06 00:50:45 -0800

    verbose mode for multi-part

--- calculate-multipart-ranges.sh
+++ calculate-multipart-ranges.sh
@@ -6,20 +6,30 @@ then
         exit 0
 fi
 
+if [[ "$*" =~ .*\-\-verbose.* ]] || [[ "$*" =~ .*\ \-v\ ?.* ]]
+then
+    VERBOSE="y"
+else
+    VERBOSE="n"
+fi
+
 #FILES=`ls $1*`
 RANGE_END=-1
 
 for f in $1*
 do
         FILE_SIZE=`wc -c $f | awk '{print $1}' `
-        echo "$f size: $FILE_SIZE"
         RANGE_START=$((RANGE_END+1))
         RANGE_END=$((RANGE_START+FILE_SIZE-1))
         RANGE=" --range \"bytes $RANGE_START-$RANGE_END/*\" "
-        echo "$RANGE"
 
-        echo ""
-        echo "Example command: aws glacier upload-multipart-part --account-id - --vault-name VAULT --body $f $RANGE --upload-id UPLOADID" 
+        if [ "$VERBOSE" = "y" ]; 
+        then 
+                echo "$f size: $FILE_SIZE"
+                echo "$RANGE"
+                echo "Example command:"
+        fi
 
+        echo "aws glacier upload-multipart-part --account-id - --vault-name \$VAULT --body $f $RANGE --upload-id \$UPLOAD" 
 done
 
