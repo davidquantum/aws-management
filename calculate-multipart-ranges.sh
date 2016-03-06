cb6686fff578eccabb7bfe7201c3cdf8e900fdde
David Pugal <david.pugal@gmail.com>
2016-03-05 23:19:07 -0800

    Fix the range calculation logic

--- calculate-multipart-ranges.sh
+++ calculate-multipart-ranges.sh
@@ -2,18 +2,19 @@
 
 if [ "$1" == "" ]
 then
-        echo "Specify root file name of of the multi part upload."
+        echo "Specify root file name (without wildcard) of of the multi part upload."
         exit 0
 fi
 
-FILES=`ls $1*`
-RANGE_START=-1
-for f in $FILES
+#FILES=`ls $1*`
+RANGE_END=-1
+
+for f in $1*
 do
         FILE_SIZE=`wc -c $f | awk '{print $1}' `
         echo "$f size: $FILE_SIZE"
-        RANGE_END=$((RANGE_START+FILE_SIZE))
-        RANGE_START=$((RANGE_START+1))
+        RANGE_START=$((RANGE_END+1))
+        RANGE_END=$((RANGE_START+FILE_SIZE-1))
         RANGE=" --range \"bytes $RANGE_START-$RANGE_END/*\" "
         echo "$RANGE"
 
